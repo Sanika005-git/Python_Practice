e780e9b8173d3ad94f3e45ed9a12fb94d54e57dd
Sanika Desai <sanikadesai382@gmail.com>
2026-03-14 01:09:29 +0530

    First commit

---
+++ 01-def-statement-demo.py
@@ -0,0 +1,12 @@
+def DisplayBirthdayWishes(personName):
+    print('Happy Birthday,')
+    print(personName, '!')
+
+
+DisplayBirthdayWishes('Sanika')
+DisplayBirthdayWishes('Sai')
+DisplayBirthdayWishes('Jui')
+
+DisplayBirthdayWishes(personName = 'Sanika')
+DisplayBirthdayWishes(personName = 'Sai')
+DisplayBirthdayWishes(personName = 'Jui')
---
+++ 01-if-statement.py
@@ -0,0 +1,13 @@
+print('Start of program')
+
+num = 100
+result = num
+a = int(input('enter a integer:'))
+
+if a > 0:
+    result = num + a
+    print('input number is positive')
+
+print('result:', result)
+print('end of program')
+
